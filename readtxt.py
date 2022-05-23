233a6bd8342cbe0037ac4cc76511ba63ad1b514d
Park Jeong Bin <61408680+wjdqlsdlsp@users.noreply.github.com>
2022-05-23 13:50:42 +0900

    Update readtxt.py

--- readtxt.py
+++ readtxt.py
@@ -8,17 +8,18 @@ os.environ['AWS_PROFILE'] = "Profile1"
 sqs = boto3.client('sqs', region_name='ap-northeast-2')
 queue_url = "https://sqs.ap-northeast-2.amazonaws.com/996145069080/from-edge"
 
-prev_x_line = ""
+prev_line = -1
 
 def send_message(message):
     global prev_line
+    now = float(message.split()[6])
 
-    if prev_line == message:
+    if prev_line-1 < now < prev_line+1:
         pass
     else:
         response = sqs.send_message(QueueUrl=queue_url,
                     MessageBody=message)
-        prev_line=message
+        prev_line=now
 
 c = 0
 file = open("./label_outs.txt","r")
